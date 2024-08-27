281da550de1b461963063bd4a650fdce6af7ce19
Pedro <pedroteixeirati@hotmail.com>
2024-08-27 14:41:25 -0300

    feat: system settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProductsModule } from './modules/products/products.module';
+import { SchedulerModule } from './modules/scheduler/scheduler.module';
+import { ImportHistoryModule } from './modules/import-history/import-history.module';
+import { HealthModule } from './modules/helth/helth.module';
 
 @Module({
   imports: [
@@ -14,19 +17,23 @@ import { ProductsModule } from './modules/products/products.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
-        autoLoadEntities: true,
-      } as TypeOrmModuleOptions),
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: 'mysql',
+          host: configService.get<string>('DB_HOST'),
+          port: +configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true,
+          autoLoadEntities: true,
+        }) as TypeOrmModuleOptions,
     }),
-    ProductsModule
+    ProductsModule,
+    SchedulerModule,
+    ImportHistoryModule,
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
